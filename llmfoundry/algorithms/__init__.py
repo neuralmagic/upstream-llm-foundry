cf3b12c6fedaff3eb8888dd3189008d7f84752cf
Eldar Kurtic <eldarkurtic@users.noreply.github.com>
2024-03-14 11:10:57 +0000

    Mask pruned weights and KD

--- llmfoundry/algorithms/__init__.py
+++ llmfoundry/algorithms/__init__.py
@@ -14,3 +14,22 @@ algorithms.register('gradient_clipping', func=GradientClipping)
 algorithms.register('alibi', func=Alibi)
 algorithms.register('gated_linear_units', func=GatedLinearUnits)
 algorithms.register('low_precision_layernorm', func=LowPrecisionLayerNorm)
+
+from composer.core import Algorithm, Event
+import torch
+class MaskPrunedWeights(Algorithm):
+    def match(self, event, state):
+        # masking weights after optimizer step should be sufficient
+        # if we detect weird behaviour, we can also do it before the forward pass
+        # by adding `or event == Event.BATCH_START`
+        return event == Event.BATCH_END
+
+    @torch.no_grad()
+    def apply(self, event, state, logger):
+        def mask_weights(module):
+            if hasattr(module, 'mask'):
+                module.weight *= module.mask
+
+        state.model.apply(mask_weights)
+
+algorithms.register('mask_pruned_weights', func=MaskPrunedWeights)
