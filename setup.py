8ae228acb9dc13170d1474d90eea4ffce1be34fd
Eldar Kurtic <eldarkurtic314@gmail.com>
2024-03-14 14:13:30 +0100

    Flash 2.5.6 breaks at H100, downgrade to 2.5.5

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ extra_deps['tensorboard'] = [
 
 # Flash 2 group kept for backwards compatibility
 extra_deps['gpu-flash2'] = [
-    'flash-attn==2.5.6',
+    'flash-attn==2.5.5',
 ]
 
 extra_deps['gpu'] = copy.deepcopy(extra_deps['gpu-flash2'])
