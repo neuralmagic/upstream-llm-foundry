1f51479832b8c64d4a59f3c86ec9aab527d98f5b
Eldar Kurtic <eldarkurtic@users.noreply.github.com>
2024-03-28 10:30:21 +0000

    add more

--- scripts/data_prep/convert_custom_finetuning.sh
+++ scripts/data_prep/convert_custom_finetuning.sh
@@ -9,9 +9,14 @@ export ELDAR_DEBUG=1
     # "jondurbin/bagel-v0.3": bagel_v03,
     # "stingning/ultrachat": ultrachat,
 
-python3 convert_custom_dataset.py \
-  --dataset cognitivecomputations/dolphin \
-  --out_root /root/datasets/opt125m_seq2k/dolphin --splits train \
-  --data_subset flan5m-alpaca-uncensored \
-  --concat_tokens 2048 --tokenizer facebook/opt-125m --no_wrap --compression zstd
+# python3 convert_custom_dataset.py \
+#   --dataset cognitivecomputations/dolphin \
+#   --out_root /root/datasets/opt125m_seq2k/dolphin --splits train \
+#   --data_subset flan5m-alpaca-uncensored \
+#   --concat_tokens 2048 --tokenizer facebook/opt-125m --no_wrap --compression zstd
 
+
+python3 convert_custom_dataset.py \
+  --dataset /network/eldar/datasets/ARC-V1-Feb2018-2/for_llmfoundry \
+  --out_root /root/arc_corpus --splits train \
+  --concat_tokens 4096 --tokenizer meta-llama/Llama-2-7b-hf --no_wrap --compression zstd
