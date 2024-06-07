e4abfbdb7478a494015831ccbc69a8e51eff3388
Eldar Kurtic <eldarkurtic314@gmail.com>
2024-06-07 16:41:54 -0400

    add yaml for gauntlet

--- scripts/eval/run.sh
+++ scripts/eval/run.sh
@@ -7,25 +7,18 @@ export WANDB_DISABLED=False
 export WANDB_PROJECT=test_evals
 
 export USE_FUSED_CROSSENTROPY_LOSS=1
-export MAX_SEQ_LEN=2048
-export PER_DEVICE_BS=16
+export MAX_SEQ_LEN=4096
+export PER_DEVICE_BS=8
 
-export BASE_PATH=/network/eldar
-export PROJECT=cerebras
-export RUN_NAME=openllm_seqlen${MAX_SEQ_LEN}_perdeviceBS${PER_DEVICE_BS}_worldsize4
+export PROJECT=sparse_llama3_8b
+export RUN_NAME=evalgauntletv03nogsm8k_seqlen${MAX_SEQ_LEN}_perdeviceBS${PER_DEVICE_BS}_worldsize4
 
-export BASE_PATH=/nfs/scistore19/alistgrp/ekurtic/eldar-upstream/llmfoundry_checkpoints
-export PROJECT=llama2_7b_cosmopedia_alldownstream
-export RUN_NAME=gauntletv03_seqlen${MAX_SEQ_LEN}_perdeviceBS${PER_DEVICE_BS}_worldsize4
+export CKPT=DownFixedMathInstCosmoFineWebBoth10BT_oneshot_wEOS_sp24_amp_bf16_maxseq8192_1ep_cosineLR1e-4_warmup1000ba_GradClip2_globalBS128_evalInterval10000ba_wEOS_noWrap
+echo $CKPT
+mkdir -p eval/results/${PROJECT}/${CKPT}
 
-for CKPT in oneshot_sp70_fromConverged24_fromConverged70onDolma_amp_bf16_maxseq4096_1ep_cosineLR1e-4_warmup1500ba_noGradClip_globalBS256_evalInterval10000ba;
-do
-    echo $CKPT
-    mkdir -p eval/${PROJECT}/${CKPT}
-
-    composer eval/eval.py \
-        eval/yamls/eval_openllm.yaml \
-        max_seq_len=${MAX_SEQ_LEN} \
-        device_eval_batch_size=${PER_DEVICE_BS} \
-        ckpt_path=${BASE_PATH}/${PROJECT}/${CKPT} 2>&1 | tee eval/${PROJECT}/${CKPT}/${RUN_NAME}.txt
-done
+composer eval/eval.py \
+    eval/yamls/eval_evalgauntletv03_noGSM8k.yaml \
+    max_seq_len=${MAX_SEQ_LEN} \
+    device_eval_batch_size=${PER_DEVICE_BS} \
+    ckpt_path=/network/eldar/llmfoundry_checkpoints/llama3_8b_cosmopedia_alldownstream/${CKPT} 2>&1 | tee eval/results/${PROJECT}/${CKPT}/${RUN_NAME}.txt
