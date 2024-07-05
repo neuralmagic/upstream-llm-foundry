1a2c10da33b97724c26a6dd2306d4cfe0388bb4f
Eldar Kurtic <eldarkurtic314@gmail.com>
2024-07-05 10:35:32 -0400

    add a bunch of examples

--- scripts/data_prep/convert_custom_pretraining.sh
+++ scripts/data_prep/convert_custom_pretraining.sh
@@ -3,8 +3,27 @@
 #git clone https://huggingface.co/datasets/cerebras/SlimPajama-627B /home/eldar/llama2_7b_SlimPajama_seq4k/ &&
 export ELDAR_DEBUG=1
 
-python3 convert_custom_dataset.py \
-  --dataset /home/eldar/SlimPajama-627B \
-  --out_root /home/eldar/datasets/llama2_7b_SlimPajama_seq4k_tokenized --splits train validation \
-  --concat_tokens 4096 --tokenizer meta-llama/Llama-2-7b-hf --no_wrap --compression zstd
+# old Llama-3 where we add BOS token to split concatenated sequences
+# python3 convert_custom_dataset.py \
+#   --dataset /nm/drive0/eldar/datasets/cosmopedia --data_files "data/**/*" --splits train \
+#   --out_root /nm/drive0/eldar/datasets/llama3_8b_Cosmopedia_seq8k_tokenized \
+#   --concat_tokens 8192 --tokenizer meta-llama/Meta-Llama-3-8B --no_wrap --compression zstd
 
+# new Llama-3 where we add EOS token to split concatenated sequences
+# NOTE: with tokenizer_call_kwargs we disable BOS/EOS completely, and with eos_text we add it manually
+# python3 convert_custom_dataset.py \
+#   --dataset /nm/drive0/eldar/datasets/cosmopedia --data_files "data/**/*" --splits train \
+#   --out_root /nm/drive0/eldar/datasets/llama3_8b_Cosmopedia_seq8k_tokenized_wEOS_yesWrap \
+#   --concat_tokens 8192 --tokenizer meta-llama/Meta-Llama-3-8B --compression zstd \
+#   --eos_text "<|end_of_text|>" --tokenizer_call_kwargs "{\"add_special_tokens\": false}"
+
+# NOTE: here we tokenize each subset independently
+# Cosmopedia subsets = {auto_math_text, khanacademy, openstax, stanford, stories, web_samples_v1, web_samples_v2, wikihow}
+for SUBSET in auto_math_text khanacademy openstax stanford stories web_samples_v1 web_samples_v2 wikihow;
+do
+    python3 convert_custom_dataset.py \
+    --dataset /nm/drive0/eldar/datasets/cosmopedia --data_subset ${SUBSET} --splits train \
+    --out_root llama3_8b_Cosmopedia_seq8k_tokenized_wEOS_noWrap_subsets/${SUBSET} --no_wrap \
+    --concat_tokens 8192 --tokenizer meta-llama/Meta-Llama-3-8B --compression zstd \
+    --eos_text "<|end_of_text|>" --tokenizer_call_kwargs "{\"add_special_tokens\": false}"
+done
--- scripts/eval/run2.sh
+++ scripts/eval/run2.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# export HF_DATASETS_CACHE="/localhome/ekurtic/hf_cache"
-
 export WANDB_ENTITY=eldarkurtic
 export WANDB_DISABLED=False
 export WANDB_PROJECT=test_evals
@@ -13,7 +11,7 @@ export PER_DEVICE_BS=8
 export PROJECT=sparse_llama3_8b
 export RUN_NAME=evalgauntletv03nogsm8k_seqlen${MAX_SEQ_LEN}_perdeviceBS${PER_DEVICE_BS}_worldsize4
 
-export CKPT=DownFixedShubhraDedup_SYN70BwdiffsOpenLLM4x_SYN70Bopenmath1x_oneshot_wEOS_sp24_amp_bf16_maxseq8192_1ep_cosineLR1e-4_warmup300ba_GradClip2_globalBS128_evalInterval3000ba_wEOS_noWrap
+export CKPT=Meta-Llama-3-8B
 echo $CKPT
 mkdir -p eval/results/${PROJECT}/${CKPT}
 
@@ -21,5 +19,4 @@ composer eval/eval.py \
     eval/yamls/eval_evalgauntletv03_noGSM8k.yaml \
     max_seq_len=${MAX_SEQ_LEN} \
     device_eval_batch_size=${PER_DEVICE_BS} \
-    ckpt_path=/network/eldar/llmfoundry_checkpoints/llama3_8b_cosmopedia_alldownstream/${CKPT} 2>&1 | tee eval/results/${PROJECT}/${CKPT}/${RUN_NAME}.txt
-
+    ckpt_path=meta-llama/${CKPT} 2>&1 | tee eval/results/${PROJECT}/${CKPT}/${RUN_NAME}.txt
