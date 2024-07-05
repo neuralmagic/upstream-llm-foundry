3a346f4519f9f552b3274acb11ef07b6345cc585
Eldar Kurtic <eldarkurtic314@gmail.com>
2024-07-05 10:21:39 -0400

    Add examples for data prep

--- scripts/data_prep/tmp7.sh
+++ scripts/data_prep/tmp7.sh
@@ -3,7 +3,7 @@
 export ELDAR_DEBUG=1
 
 python3 convert_custom_dataset.py \
-    --dataset prigoyal/flan2021_submix_original --splits train \
-    --out_root /nm/drive0/eldar/datasets/llama3_8b/seqlen8k_tokenized_wEOS_yesWrap/flan2021_submix_original \
+    --dataset HuggingFaceFW/fineweb-edu --splits train --no_wrap --data_subset sample-10BT \
+    --out_root /home/eldar/datasets/fineweb_edu_sample10BT/llama3_8b_FineWebEduSample10BT_seq8k_tokenized_wEOS_noWrap \
     --concat_tokens 8192 --tokenizer meta-llama/Meta-Llama-3-8B --compression zstd \
     --eos_text "<|end_of_text|>" --tokenizer_call_kwargs "{\"add_special_tokens\": false}"
